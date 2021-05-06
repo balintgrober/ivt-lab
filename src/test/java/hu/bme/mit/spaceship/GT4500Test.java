14af576a60a08ec0ed33701da9de878f50421b80
balintgrober <groberbalint@gmail.com>
2021-05-06 22:46:11 +0200

    fixed test with mockito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -10,31 +10,44 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+  TorpedoStore mock_primaryStore;
+  TorpedoStore mock_secondaryStore;
+
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mock_primaryStore = mock(TorpedoStore.class);
+    mock_secondaryStore = mock(TorpedoStore.class);
+    this.ship = new GT4500(mock_primaryStore, mock_secondaryStore);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    when(mock_primaryStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(mock_primaryStore, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    when(mock_primaryStore.fire(1)).thenReturn(true);
+    when(mock_secondaryStore.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+
+    verify(mock_primaryStore, times(1)).fire(1);
+    verify(mock_secondaryStore, times(1)).fire(1);
   }
 
 }
